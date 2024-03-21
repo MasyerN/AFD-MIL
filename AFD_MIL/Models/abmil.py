30800f982e7e65278479812dcd302c351632880a
MasyerN <141401919+MasyerN@users.noreply.github.com>
2024-03-21 21:59:37 +0800

    abmil.py

--- AFD_MIL/Models/abmil.py
+++ AFD_MIL/Models/abmil.py
@@ -83,62 +83,7 @@ class MixABMIL(nn.Module):
         return y, attn.transpose(-1, -2).squeeze(0).squeeze(0)
 
 
-class Stable_ABMIL(nn.Module):
-    def __init__(self, cfgs):
-        super(Stable_ABMIL, self).__init__()
-        self.L = cfgs['feature_dim']
-        self.D = int(cfgs['feature_dim'] / 2)
-        self.K = 1
 
-        self.encoder = MeanPoolingMIL(cfgs)
-
-        self.attention = nn.Sequential(
-            nn.Linear(self.L, self.D),
-            nn.Dropout(.2),
-            nn.Tanh(),
-            nn.Linear(self.D, self.K)
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(self.L*self.K, cfgs["num_classes"]),
-            # nn.Softmax(dim=-1)
-            # nn.Sigmoid()
-        )
-        self.register_buffer('pre_features', torch.zeros(cfgs["k"], cfgs['feature_dim']))
-        self.register_buffer('pre_weight1', torch.ones(cfgs["k"], 1))
-    def forward(self, x, weight):
-
-        _, x = self.encoder(x[0])  # BxLxN
-
-        attn = self.attention(x.unsqueeze(0)).softmax(dim=1) # BxLx1
-        attn = torch.mul(attn, weight)
-        m = torch.matmul(attn.transpose(-1, -2), x).squeeze(1)  # Bx1xN
-        y = self.classifier(m)
-        # y_hat = torch.ge(y_prob, 0.5).float()
-        return y, _
-
-    # AUXILIARY METHODS
-    def inference(self, x, weight):
-        y, _ = self.forward(x, weight)
-        #y = y.argmax(dim=-1)
-        return y
-    """
-    def calculate_classification_error(self, X, Y):
-        Y = Y.float()
-        _, Y_hat, _ = self.forward(X)
-        error = 1. - Y_hat.eq(Y).cpu().float().mean().data.item()
-
-        return error, Y_hat
-
-    def calculate_objective(self, X, Y):
-        Y = Y.float()
-        Y_prob, _, A = self.forward(X)
-        print(Y_prob,A)
-        Y_prob = torch.clamp(Y_prob, min=1e-5, max=1. - 1e-5)
-        neg_log_likelihood = -1. * (Y * torch.log(Y_prob) + (1. - Y) * torch.log(1. - Y_prob))  # negative log bernoulli
-
-        return neg_log_likelihood, A
-    """
 
 class GatedAttention(nn.Module):
     def __init__(self, cfgs):
@@ -200,7 +145,7 @@ class MeanPoolingMIL(nn.Module):
         self.ln = nn.LayerNorm(cfgs['feature_dim'])
         self.classifier = nn.Linear(cfgs['feature_dim'], cfgs["num_classes"])
         self.cfg = cfgs
-        #self.attentionnet = ABMIL(cfgs)
+        self.attentionnet = ABMIL(cfgs)
         
     def forward(self, x):
         x = self.dropout(self.embedding(x))
@@ -210,53 +155,7 @@ class MeanPoolingMIL(nn.Module):
         return y, x
     
     def inference(self, x):
-        #atten_target, atten_score = self.attentionnet(x)
+        atten_target, atten_score = self.attentionnet(x)
         x = x.reshape([-1, self.cfg['feature_dim']])
         y, _ = self.forward(x)
-        return torch.softmax(y, 1)#, atten_target, atten_score[0]
-    
-    
-    
-class IRL_ABMIL(nn.Module):
-    def __init__(self, cfgs):
-        super(IRL_ABMIL, self).__init__()
-        self.L = cfgs['feature_dim']
-        self.D = int(cfgs['feature_dim'] / 2)
-        self.K = 1
-
-        self.encoder = MeanPoolingMIL(cfgs)
-
-        self.attention = nn.Sequential(
-            nn.Linear(self.L, self.D),
-            nn.Dropout(.2),
-            nn.Tanh(),
-            nn.Linear(self.D, self.K)
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(self.L*self.K, cfgs["num_classes"]),
-            # nn.Softmax(dim=-1)
-            # nn.Sigmoid()
-        )
-    def forward(self, x):
-        _, x = self.encoder(x[0])
-        attn = self.attention(x.unsqueeze(0)).softmax(dim=1)
-        m = torch.matmul(attn.transpose(-1, -2), x).squeeze(1)  # Bx1xN
-        y = self.classifier(m)
-        # y_hat = torch.ge(y_prob, 0.5).float()
-        return y, _
-
-    # AUXILIARY METHODS
-    def inference(self, x):
-        y, _ = self.forward(x)
-        y = y.softmax(dim=-1)
-        pred = y.argmax(dim=-1)
-        return pred, y
-    
-    def inference2(self, x):
-        _, x = self.encoder(x[0])
-        attn = self.attention(x.unsqueeze(0)).softmax(dim=1)
-        m = torch.matmul(attn.transpose(-1, -2), x).squeeze(1)  # Bx1xN
-        y = self.classifier(m)
-        y = y.argmax(dim=-1)
-        return y, attn.transpose(-1, -2).squeeze(0).squeeze(0)
+        return torch.softmax(y, 1), atten_target, atten_score[0]
